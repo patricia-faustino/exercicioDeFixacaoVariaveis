67ff2465f182f9bd01ad1d343b8064881f8de399
Patricia <patriciafaustinobr@gmail.com>
2020-09-27 19:34:41 -0300

    Resolvi o exercicio proposto para inicializacao de variaveis e adicionei saidas na tela do console

---
+++ ConsoleApp3/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp3 {
+    class Product {
+        private string _nome;
+        public double Preco { get; private set; }//não é permitido para outros arquivos alterar
+        //o valor do atributo Preco
+        public int Quantidade { get; private set;}
+
+        public Product() {
+         
+        }
+        public Product(string nome, double preco, int quantidade)  {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public string Nome {//expor para mundo externo apenas a propriedade
+            get { return _nome; }
+            set {
+                if(value != null && value.Length > 1) {
+                    _nome = value;
+
+                }
+            }
+
+
+        }
+        public double ValorEmEstoque() {
+            return Preco * Quantidade;
+        }
+        public void Adicionar(int adicionar) {
+             Quantidade += adicionar;
+        }
+        public void Remover(int adicionar) {
+            Quantidade -= adicionar;
+        }
+        public override string ToString() {
+            return "Dados do produto: "+ _nome +
+               ", "+Preco+" , "+Quantidade+" , Total: "+
+               ValorEmEstoque();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp3 {
+    class Program {
+        static void Main(string[] args) {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+            Console.WriteLine("Produtos");
+            Console.WriteLine($"{produto1}, cujo preço é R${preco1}");
+            Console.WriteLine($"{produto2},cujo preço é R${preco2}");
+            Console.WriteLine("{0}, cujo preço é R$ {1}",produto1,preco1);
+            Console.WriteLine($"Registro:{idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine($"Medida com oito casas decimais:{medida:F8}");
+            Console.WriteLine($"Medida com oito casas decimais:{medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " +  medida.ToString("F3",CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
